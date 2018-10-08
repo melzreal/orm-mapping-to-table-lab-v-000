6ca9395d4f4c37e848b748ae16e1aba88e8241bf
Melissa Real <melissa.real@hotmail.com>
2018-10-08 20:25:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,10 +23,20 @@ attr_reader :id
 
   def self.drop_table
     sql =  <<-SQL
-      DROP TABLE students 
+      DROP TABLE students
       SQL
     DB[:conn].execute(sql)
   end
+
+  def save
+     sql = <<-SQL
+       INSERT INTO students (name, grade)
+       VALUES (?, ?)
+     SQL
+
+   DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end 
 
 
 
